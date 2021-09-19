05d45950686a53c308a8262c87783a32a217a9e8
ivan <van.emelin2012@yandex.ru>
2021-09-19 20:19:22 +0300

    refactored demmel lu

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,24 +2,26 @@
 
 int main()
 {
-    int size = 3;
-    double *a = new double[size * size];
-    double *b = new double[size];
-    readFromFile(a, "input_data/a.txt");
-    readFromFile(b, "input_data/b.txt");
-    //fillMatrixRandom(a, size);
+    int sizeN = 5;
+    int sizeM = 5;
+    double *a = new double[sizeM * sizeN];
+    double *b = new double[sizeM];
+    //readFromFile(a, "input_data/a.txt");
+    //readFromFile(b, "input_data/b.txt");
+    fillMatrixRandom(a, sizeM, sizeN);
     //fillVectorRandom(b, size);
 
-    DemmelLuSolver demmelLuSolver = DemmelLuSolver(size);
+    DemmelLuSolver demmelLuSolver = DemmelLuSolver(sizeN, sizeM);
     demmelLuSolver.setA(a);
+    //demmelLuSolver.findDecomposition(0, 0, 4, 4);
     demmelLuSolver.findDecomposition();
     demmelLuSolver.printDecomposition();
 
-    LUSolver luSolver = LUSolver(size);
+    LUSolver luSolver = LUSolver(sizeN);
 
     luSolver.findDecomposition(a);
     luSolver.printDecomposition();
 
-    double *res = luSolver.solveWith(b);
-    printVector(res, size, "Result");
+    //double *res = luSolver.solveWith(b);
+    //printVector(res, size, "Result");
 }
