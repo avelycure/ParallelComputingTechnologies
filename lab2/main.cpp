64987d4dc7c525ff543bb2986f88ff216b1eefb8
Vasiliy Molochkov <vasya.molochkof@gmail.com>
2021-10-21 21:58:37 +0300

    added better generation for initial array, its size increased

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -6,6 +6,7 @@
 #include <random>
 #include <algorithm>
 #include <chrono>
+#include <fopenmp>
 
 double EPS = 1e-6;
 
@@ -151,20 +152,20 @@ bool is_close(double x, double y)
     }
 }
 
-void get_sample(body* arr, size_t arr_size, body* sample, size_t sample_size)
-{
-    body* temp_arr = new body[arr_size];
-    std::copy(arr, arr + arr_size, temp_arr);
-    std::shuffle(temp_arr, temp_arr + arr_size, std::default_random_engine(42));
-    std::copy(temp_arr, temp_arr + sample_size, sample);
-    delete[] temp_arr;
-}
+// void get_sample(body* arr, size_t arr_size, body* sample, size_t sample_size)
+// {
+//     body* temp_arr = new body[arr_size];
+//     std::copy(arr, arr + arr_size, temp_arr);
+//     std::shuffle(temp_arr, temp_arr + arr_size, std::default_random_engine(42));
+//     std::copy(temp_arr, temp_arr + sample_size, sample);
+//     delete[] temp_arr;
+// }
 
 int main()
 {
     srand(42);
-    size_t n = 1000; //array size
-    size_t n_small = 400; //subarray size
+    size_t n = 9000000; //array size
+    size_t n_small = n/3 - 1; //subarray size
     body* mass = new body[n];
     body* submass = new body[n_small];
     BadTree *tree;
@@ -172,14 +173,27 @@ int main()
     //timers
     auto begin = std::chrono::steady_clock::now();
     auto end = std::chrono::steady_clock::now();
+    auto build_tree_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
     auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
 
     for(size_t i = 0; i < n; ++i)
     {
-        mass[i] = {-250 + 500*((double)rand() / RAND_MAX), (double)rand() / RAND_MAX * 1000};
+        mass[i] = {-250.1 + 3.6436457 * i, 1.3 * i};
     }
-    get_sample(mass, n, submass, n_small);
+    // get_sample(mass, n, submass, n_small);
+    std::shuffle(mass, mass + n,std::default_random_engine(42));
+
+    for(size_t i = 0; i < n_small; ++i)
+    {
+        submass[i] = mass[3*i];
+    }
+
+
+    begin = std::chrono::steady_clock::now();
     tree =  new BadTree(mass, n);
+    end = std::chrono::steady_clock::now();
+    build_tree_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    std::cout << "build tree time: " << build_tree_time.count() << "\n";
 
     // std::cout << "init array:\n";
     // for(size_t i = 0; i < n; ++i)
@@ -223,7 +237,7 @@ int main()
     end = std::chrono::steady_clock::now();
     elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin); 
     std::cout << "sum tree: " << sumTree << "\n";
-    std::cout << "time ms: " << elapsed_ms.count() << "\n";
+    std::cout << "finding sum ms: " << elapsed_ms.count() << "\n";
 
     double sumMas = 0;
     for(size_t i = 0; i < n_small; ++i)
@@ -233,5 +247,6 @@ int main()
     std::cout << "sum mass: " << sumMas << "\n";
 
     delete[] mass;
+    delete[] submass;
     delete tree;
 }
