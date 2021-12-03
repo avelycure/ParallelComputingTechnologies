3db9225171b3cf895015d1d8d70ff34238b0ff41
avelycure <van.emelin2012@yandex.ru>
2021-12-03 18:10:44 +0300

    chanched size

--- lab3/init_conds/InitialConditions.hpp
+++ lab3/init_conds/InitialConditions.hpp
@@ -7,7 +7,7 @@
 class InitialConditions
 {
 public:
-    int n = 128 * 16 - 1;
+    int n = 128 * 128 - 1;
     double leftBorder = 0.0;
     double rightBorder = 1.0;
     double h = (rightBorder - leftBorder) / n;
