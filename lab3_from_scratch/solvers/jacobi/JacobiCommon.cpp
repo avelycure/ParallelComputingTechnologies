e4d0275f5b4d7bd2c8806cd36731dacd0bca7e15
avelycure <van.emelin2012@yandex.ru>
2021-12-05 15:38:35 +0300

    rearranged project, extracted common logic to file

---
+++ lab3_from_scratch/solvers/jacobi/JacobiCommon.cpp
@@ -0,0 +1,75 @@
+#include "Jacobi.hpp"
+
+/**
+ * Copy first row of local part of solution
+ * This function is needed for transmition
+ * */
+void copyFirstRow(std::vector<double> &yLocal,
+                  std::vector<double> &localHighBorder,
+                  InitialConditions initialConditions)
+{
+    for (int i = 0; i < initialConditions.n; i++)
+        localHighBorder[i] = yLocal[i];
+}
+
+/**
+ * Copy last row of local part of solution
+ * This function is needed for transmition
+ * */
+void copyLastRow(std::vector<double> &yLocal,
+                 std::vector<double> &localLowBorder,
+                 InitialConditions initialConditions)
+{
+    for (int i = 0; i < initialConditions.n; i++)
+        localLowBorder[i] = yLocal[yLocal.size() - initialConditions.n + i];
+}
+
+
+/**
+ * Solving the linear system of equations with 5 diagonal matrix which we get trying to solve
+ * Helmholtz equation in square
+ * */
+void solveSystem(
+    std::vector<double> &yLocal,
+    std::vector<double> &yLocalPrevious,
+    std::vector<double> &yLocalPreviousUpHighBorder,
+    std::vector<double> &yLocalPreviousDownLowBorder,
+    int numberOfProcesses,
+    int processId,
+    int localRows,
+    int localSize,
+    int localOffsetInRows,
+    InitialConditions initialConditions)
+{
+    //Local renaming to increase readability
+    double h = initialConditions.h;
+    int n = initialConditions.n;
+    //Just coefficient of the equation
+    double c = 1.0 / (4.0 + initialConditions.h * initialConditions.h * initialConditions.k * initialConditions.k);
+
+    if (processId != 0)
+        for (int j = 1; j < n - 1; j++)
+            yLocal[j] = c * (h * h * initialConditions.f(localOffsetInRows * h, j * h) +
+                             yLocalPreviousUpHighBorder[j] +
+                             yLocalPrevious[n + j] +
+                             yLocalPrevious[j - 1] +
+                             yLocalPrevious[j + 1]);
+    //Else if it is first process do nothing because initial conditions in the first row are zeros
+
+    //Calculate only rows that are not borders of process part
+    for (int i = 1; i < localRows - 1; i++)
+        for (int j = 1; j < n - 1; j++)
+            yLocal[i * n + j] = c * (h * h * initialConditions.f((localOffsetInRows + i) * h, j * h) +
+                                     yLocalPrevious[(i - 1) * n + j] +
+                                     yLocalPrevious[(i + 1) * n + j] +
+                                     yLocalPrevious[i * n + (j - 1)] +
+                                     yLocalPrevious[i * n + (j + 1)]);
+
+    if (processId != numberOfProcesses - 1)
+        for (int j = 1; j < n - 1; j++)
+            yLocal[localSize - n + j] = c * (h * h * initialConditions.f((localOffsetInRows + localRows - 1) * h, j * h) +
+                                             yLocalPrevious[localSize - 2 * n + j] +
+                                             yLocalPreviousDownLowBorder[j] +
+                                             yLocalPrevious[localSize - n + j - 1] +
+                                             yLocalPrevious[localSize - n + j + 1]);
+}
--- lab3_from_scratch/solvers/jacobi/JacobiV1.cpp
+++ lab3_from_scratch/solvers/jacobi/JacobiV1.cpp
@@ -145,30 +145,6 @@ void jacobiV1(
 }
 
 /**
- * Copy first row of local part of solution
- * This function is needed for transmition
- * */
-void copyFirstRow(std::vector<double> &yLocal,
-                  std::vector<double> &localHighBorder,
-                  InitialConditions initialConditions)
-{
-    for (int i = 0; i < initialConditions.n; i++)
-        localHighBorder[i] = yLocal[i];
-}
-
-/**
- * Copy last row of local part of solution
- * This function is needed for transmition
- * */
-void copyLastRow(std::vector<double> &yLocal,
-                 std::vector<double> &localLowBorder,
-                 InitialConditions initialConditions)
-{
-    for (int i = 0; i < initialConditions.n; i++)
-        localLowBorder[i] = yLocal[yLocal.size() - initialConditions.n + i];
-}
-
-/**
  * We have to exchange two rows: the first and the last. We have two buffers to simplify readability: @buf1, @buf2.
  * Firstly we send from top of the matrix to the bottom. The schema is like this: 0->1->2->3->... where numbers are processes ranks
  * Then we send data from bottom to the top: 4->3->2->1->0. Each process(except 0 and numberOfProcesses - 1,  which are
@@ -205,49 +181,4 @@ void exchangeData(std::vector<double> &yLocal,
     }
     if (processId != numberOfProcesses - 1)
         MPI_Recv(yLocalPreviousDownLowBorder.data(), initialConditions.n, MPI_DOUBLE, processId + 1, 41, MPI_COMM_WORLD, &statU);
-}
-
-void solveSystem(
-    std::vector<double> &yLocal,
-    std::vector<double> &yLocalPrevious,
-    std::vector<double> &yLocalPreviousUpHighBorder,
-    std::vector<double> &yLocalPreviousDownLowBorder,
-    int numberOfProcesses,
-    int processId,
-    int localRows,
-    int localSize,
-    int localOffsetInRows,
-    InitialConditions initialConditions)
-{
-    //Local renaming to increase readability
-    double h = initialConditions.h;
-    int n = initialConditions.n;
-    //Just coefficient of the equation
-    double c = 1.0 / (4.0 + initialConditions.h * initialConditions.h * initialConditions.k * initialConditions.k);
-
-    if (processId != 0)
-        for (int j = 1; j < n - 1; j++)
-            yLocal[j] = c * (h * h * initialConditions.f(localOffsetInRows * h, j * h) +
-                             yLocalPreviousUpHighBorder[j] +
-                             yLocalPrevious[n + j] +
-                             yLocalPrevious[j - 1] +
-                             yLocalPrevious[j + 1]);
-    //Else if it is first process do nothing because initial conditions in the first row are zeros
-
-    //Calculate only rows that are not borders of process part
-    for (int i = 1; i < localRows - 1; i++)
-        for (int j = 1; j < n - 1; j++)
-            yLocal[i * n + j] = c * (h * h * initialConditions.f((localOffsetInRows + i) * h, j * h) +
-                                     yLocalPrevious[(i - 1) * n + j] +
-                                     yLocalPrevious[(i + 1) * n + j] +
-                                     yLocalPrevious[i * n + (j - 1)] +
-                                     yLocalPrevious[i * n + (j + 1)]);
-
-    if (processId != numberOfProcesses - 1)
-        for (int j = 1; j < n - 1; j++)
-            yLocal[localSize - n + j] = c * (h * h * initialConditions.f((localOffsetInRows + localRows - 1) * h, j * h) +
-                                             yLocalPrevious[localSize - 2 * n + j] +
-                                             yLocalPreviousDownLowBorder[j] +
-                                             yLocalPrevious[localSize - n + j - 1] +
-                                             yLocalPrevious[localSize - n + j + 1]);
 }
