6c9f68d914c64aaa655ccdaabcc9741593d8095b
Vasiliy Molochkov <vasya.molochkof@gmail.com>
2021-12-26 14:34:15 +0300

    added test

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -39,6 +39,11 @@ point point_substraction(point p1, point p2)
     return {p1.x-p2.x, p1.y-p2.y, p1.z-p2.z};
 }
 
+point point_sum(point p1, point p2)
+{
+    return {p1.x+p2.x, p1.y+p2.y, p1.z+p2.z};
+}
+
 point point_by_double(point p, double d)
 {
     return {p.x * d, p.y * d, p.z * d};
@@ -83,6 +88,7 @@ public:
     std::vector<left_> bodys;
     double t;
     unsigned int iter;
+    bool test_flag;
 
     NBody(size_t, double, unsigned int, bool);
     ~NBody();
@@ -91,14 +97,49 @@ public:
     void solve();
 };
 
-NBody::NBody(size_t n, double t_ = 0.001, unsigned int iter_ = 100, bool test_flag=false)
+NBody::NBody(size_t n = 4, double t_ = 0.001, unsigned int iter_ = 100, bool test_flag_=false)
 {
     t = t_;
     iter = iter_;
+    test_flag = test_flag_;
 
     if(test_flag)
     {
         std::cout << "test\n";
+
+        bodys.resize(4);
+        // 8810324116.227   1.0  0.0  0.0   0.0  0.9  0.0
+        bodys[0].m = 8810324116.227;
+        bodys[0].r.x = 1;
+        bodys[0].r.y = 0;
+        bodys[0].r.z = 0;
+        bodys[0].v.x = 0;
+        bodys[0].v.y = 0.9;
+        bodys[0].v.z = 0;
+
+        bodys[1].m = 8810324116.227;
+        bodys[1].r.x = 0;
+        bodys[1].r.y = 1;
+        bodys[1].r.z = 0;
+        bodys[1].v.x = -0.9;
+        bodys[1].v.y = 0;
+        bodys[1].v.z = 0;
+
+        bodys[2].m = 8810324116.227;
+        bodys[2].r.x = -1;
+        bodys[2].r.y = 0;
+        bodys[2].r.z = 0;
+        bodys[2].v.x = 0;
+        bodys[2].v.y = -0.9;
+        bodys[2].v.z = 0;
+
+        bodys[3].m = 8810324116.227;
+        bodys[3].r.x = 0;
+        bodys[3].r.y = -1;
+        bodys[3].r.z = 0;
+        bodys[3].v.x = 0.9;
+        bodys[3].v.y = 0;
+        bodys[3].v.z = 0;
     }
     else
     {
@@ -119,6 +160,8 @@ void NBody::step()
     right_ temp;
     point res;
     double multiplier;
+
+    point dr, dv;
     //calculate a
     for(int i = 0; i < bodys.size(); ++i)
     {
@@ -135,9 +178,14 @@ void NBody::step()
                 res = point_by_double(res, multiplier);
                 temp.a = point_substraction(temp.a, res);
             }
-
-            bodys[i].r = point_by_double(bodys[i].v, t);
-            bodys[i].v = point_by_double(temp.a, t);
+            dr = point_by_double(bodys[i].v, t);
+            dv = point_by_double(temp.a, t);
+            bodys[i].r = point_sum(bodys[i].r, dr);
+            bodys[i].v = point_sum(bodys[i].v, dv);
+            if(test_flag && i == 0)
+            {
+                std::cout << bodys[i].r.x << " " << bodys[i].r.y << " " << bodys[i].r.z << "\n";
+            }
         }
     }
 }
@@ -153,7 +201,7 @@ void NBody::solve()
 int main()
 {
     srand(42);
-    NBody sas(4);
+    NBody sas(4, 0.1, 100, true);
     sas.solve();
 
     return 0;
